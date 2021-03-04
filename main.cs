3719f7d418db9188088714dbd7376e1bf4ce841c
jonaerollins <jonaerollins@gmail.com>
2021-03-04 03:49:02 +0000

    the entire code

--- main.cs
+++ main.cs
@@ -4,18 +4,36 @@ class MainClass {
   public static void Main (string[] args) {
     
  Console.WriteLine("Lab2b");
+    
+    int firstnum;
+    int secondnum;
+    int sum;
+    int product;
+    int difference;
+    int quotient; 
+    int remainder;
 
- 
- int num1;
- int num2;
+        Console.Write("Enter number : ");
+        firstnum = Convert.ToInt32(Console.ReadLine());
 
- 
+        Console.Write("Enter another number : ");
+        secondnum = Convert.ToInt32(Console.ReadLine());
 
+        sum = firstnum + secondnum;
+        product = firstnum * secondnum;
+        difference = firstnum - secondnum;
+        quotient = firstnum / secondnum;
+        remainder = firstnum % secondnum;
 
-
-
-
-
+        Console.Write(" Sum = " + sum);
+        
+        Console.Write(" Product = " + product);
+       
+        Console.Write(" Difference = " + difference);
+        
+        Console.Write(" Quotient = " + quotient);
+       
+        Console.Write(" Remainder = " + remainder);
 
   }
 }
